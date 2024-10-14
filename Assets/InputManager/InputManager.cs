936fcd45c6429b2e3c7dff5f83deddf35a66d58b
Ban-GunOo <slqdydskq@naver.com>
2024-10-14 12:39:46 +0900

    first

---
+++ Assets/InputManager/InputManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    Rigidbody2D rb;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //매프레임마다 Vertical축과 Horizontal축을 가져옵니다.
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector2 direction = new Vector2(vertical, horizontal);
+        //길이를 1로 만드는 작업(정규화)
+
+        direction = direction.normalized;
+
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
